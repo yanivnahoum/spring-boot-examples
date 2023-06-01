dffcede97c2525b4c64210fabba8da7a588c04a8
Yaniv Nahoum <yaniv.nahoum@intl.att.com>
2023-06-01 08:35:46 +0300

    App skeleton

--- src/main/java/com/att/training/springboot/examples/aop/AopConfig.java
+++
@@ -1,8 +0,0 @@
-package com.att.training.springboot.examples.aop;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration(proxyBeanMethods = false)
-@EnableAspectJAutoProxy
-public class AopConfig {}
