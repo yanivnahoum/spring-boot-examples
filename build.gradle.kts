c2dc4890b55402c908580b237e88d9c78b385b45
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2023-08-06 12:34:32 +0300

    Upgraded maven, gradle and Spring Boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.1.1"
+    id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.0"
-    id("io.freefair.lombok") version "8.0.1"
+    id("io.freefair.lombok") version "8.1.0"
 }
 
 group = "com.att.training"
@@ -18,6 +18,7 @@ repositories {
     mavenCentral()
 }
 
+extra["snakeyaml.version"] = "2.0"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -29,7 +30,7 @@ dependencies {
 tasks {
     withType<JavaCompile>().configureEach {
         with(options) {
-            release.set(17)
+            release = 17
             compilerArgs.add("-Xlint:all,-processing,-auxiliaryclass")
         }
     }
