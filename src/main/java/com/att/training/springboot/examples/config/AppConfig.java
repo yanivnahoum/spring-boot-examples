0ff01e2bd00c0c7a7cb2399052495fedc28ae90c
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2023-11-13 12:53:32 +0200

    Added actuator

--- src/main/java/com/att/training/springboot/examples/config/AppConfig.java
+++ src/main/java/com/att/training/springboot/examples/config/AppConfig.java
@@ -7,12 +7,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.lang.NonNull;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import java.util.concurrent.Executor;
 
 @EnableAspectJAutoProxy
 @Configuration(proxyBeanMethods = false)
 public class AppConfig {
+    @Bean
+    CommonsRequestLoggingFilter requestLoggingFilter() {
+        var loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(200);
+        return loggingFilter;
+    }
     @Bean
     public Executor ioTaskExecutor() {
         var coreCount = Runtime.getRuntime().availableProcessors() * 64;
@@ -42,5 +52,4 @@ public class AppConfig {
             }
         };
     }
-
 }
