6fc0f8ac654f8e3bde3790b0debe8d1d1278f6db
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2023-11-27 16:59:46 +0200

    Reproduced Spring Boot 3.2.0 bug

---
+++ src/test/java/com/att/training/springboot/examples/MoreSpringBootExamplesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.att.training.springboot.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.application.name=app")
+class MoreSpringBootExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
