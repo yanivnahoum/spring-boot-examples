d9bd6d2917e1c0220d3da8acfcb71721b0c23f61
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2023-12-31 18:41:37 +0200

    Spring data mongo

--- README.md
+++ README.md
@@ -1,17 +1,10 @@
-## Spring Boot Examples
+## Spring Data Mongo
 
-This repo contains multiple branches, each an isolated Spring Boot app demonstrating the use or implementation of a
-different concern.
+### Running the app:
 
-**Branches**:
+1. Run `docker run --rm -d --name mongo -p 27017:27017 mongo`
+2. Run app and observe the log: `Insert: Person(name=Joe, age=34,...`
+3. Shutdown the app
+4. Stop and remove container: `docker stop mongo`
 
-* abstract-datasource: dynamic routing of multiple data sources
-* structured-logging: structured logging using logback and a json encoder
-* error-handling: comparing various error handling patterns
-* composition-vs-inheritance: comparing 2 software design patterns: composition vs inheritance
-* feature-management: using Azure App Configuration to manage feature flags
-* spring-libs: creating and consuming Spring libraries
-* spring-properties: defining and using Spring Boot properties
-* context-propagation: propagating thread-local context to async executions in Spring MVC
-* spring-converters: creating and using Spring converters
-* spring-threading: Parallelizing tasks using Spring's TaskExecutor, Async and CompletableFutures
+
--- src/main/java/com/att/training/springboot/examples/SpringBootExamplesApplication.java
+++ src/main/java/com/att/training/springboot/examples/SpringBootExamplesApplication.java
@@ -1,15 +1,75 @@
 package com.att.training.springboot.examples;
 
+import com.mongodb.MongoClient;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoOperations;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Update.update;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@Slf4j
 public class SpringBootExamplesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExamplesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(MongoOperations mongoOps, MongoClient mongoClient) {
+		return args -> {
+			Person person = new Person("Joe", 34);
+
+			// Insert is used to initially store the object into the database.
+			mongoOps.insert(person);
+			log.info("Insert: " + person);
+
+			// Find
+			person = mongoOps.findById(person.getId(), Person.class);
+			log.info("Found: " + person);
+
+			// Update
+			mongoOps.updateFirst(query(where("name").is("Joe")), update("age", 35), Person.class);
+			person = mongoOps.findOne(query(where("name").is("Joe")), Person.class);
+			requireNonNull(person);
+			log.info("Updated: " + person);
+
+			// Delete
+			mongoOps.remove(person);
+
+			// Check that deletion worked
+			List<Person> people = mongoOps.findAll(Person.class);
+			log.info("Number of people = " + people.size());
+
+			mongoOps.dropCollection(Person.class);
+
+			// Use the mongoClient directly:
+			log.info("Mongo databases:");
+			for (String dbName : mongoClient.listDatabaseNames()) {
+				log.info(dbName);
+			}
+		};
+	}
+}
+
+@ToString
+@RequiredArgsConstructor
+@Getter
+class Person {
+	private final String name;
+	private final int age;
+	private String id;
 }
