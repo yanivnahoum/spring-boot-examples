cdfd4a71a567a8229a0f9be6fda571685d796919
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-07-04 11:10:19 +0300

    Added bean validation

--- src/main/java/com/att/training/springboot/examples/UserClientProperties.java
+++ src/main/java/com/att/training/springboot/examples/UserClientProperties.java
@@ -1,6 +1,9 @@
 package com.att.training.springboot.examples;
 
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties("app.user")
-public record UserClientProperties(String baseUrl) {}
+@Validated
+public record UserClientProperties(@NotEmpty String baseUrl) {}
