5da58fd613a4c28454434b77458a78c506a2e81a
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-10-16 13:42:08 +0300

    Added Spring Cloud Gateway Mvc Application

---
+++ spring-cloud-gateway-mvc/src/main/java/com/att/training/springboot/examples/SpringCloudGatewayMvcApplication.java
@@ -0,0 +1,15 @@
+package com.att.training.springboot.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class SpringCloudGatewayMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayMvcApplication.class, args);
+    }
+
+}
