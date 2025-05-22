caa271c10c253dab1597580c1b6b8c528629ac4d
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-05-22 10:51:21 +0300

    Remove unnecessary @Validated annotation

--- spring-cache-redis/src/main/java/com/att/training/springboot/examples/UserController.java
+++ spring-cache-redis/src/main/java/com/att/training/springboot/examples/UserController.java
@@ -3,7 +3,6 @@ package com.att.training.springboot.examples;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
-@Validated
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
