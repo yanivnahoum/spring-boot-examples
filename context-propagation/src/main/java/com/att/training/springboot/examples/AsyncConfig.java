a1d1f9371b5d2d991a0fad5d83d670b6472e2986
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-05-25 13:22:22 +0300

    Upgraded to Spring Boot 3.5.0

--- context-propagation/src/main/java/com/att/training/springboot/examples/AsyncConfig.java
+++ context-propagation/src/main/java/com/att/training/springboot/examples/AsyncConfig.java
@@ -1,14 +1,14 @@
 package com.att.training.springboot.examples;
 
-import org.springframework.boot.task.ThreadPoolTaskExecutorCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 
 @Configuration(proxyBeanMethods = false)
 public class AsyncConfig {
     @Bean
-    public ThreadPoolTaskExecutorCustomizer taskDecorator() {
-        return taskExecutor -> taskExecutor.setTaskDecorator(this::propagateLogContext);
+    public TaskDecorator taskDecorator() {
+        return this::propagateLogContext;
     }
 
     private Runnable propagateLogContext(Runnable runnable) {
--- context-propagation/src/main/java/com/att/training/springboot/examples/SomeService.java
+++ context-propagation/src/main/java/com/att/training/springboot/examples/SomeService.java
@@ -3,18 +3,18 @@ package com.att.training.springboot.examples;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SomeService {
-    private final Executor ioTaskExecutor;
+    private final TaskExecutor taskExecutor;
 
     void sync() {
         try (var ignored = LogContext.putClosableCorrelationId()) {
@@ -27,10 +27,10 @@ public class SomeService {
         try (var ignored = LogContext.putClosableCorrelationId()) {
             log.info("#async");
             // We can wrap out runnables with a ContextRunnable:
-//            var task1 = CompletableFuture.runAsync(new ContextRunnable(() -> sleepAndLog(Duration.ofSeconds(2))), ioTaskExecutor);
-            var task1 = CompletableFuture.runAsync(() -> sleepAndLog(Duration.ofSeconds(2)), ioTaskExecutor);
-            var task2 = CompletableFuture.runAsync(() -> sleepAndLog(Duration.ofSeconds(1)), ioTaskExecutor);
-            CompletableFuture.allOf(task1, task2).join();
+            var task1 = CompletableFuture.runAsync(new ContextRunnable(() -> sleepAndLog(Duration.ofSeconds(2))), taskExecutor);
+            var task2 = CompletableFuture.runAsync(() -> sleepAndLog(Duration.ofSeconds(2)), taskExecutor);
+            var task3 = CompletableFuture.runAsync(() -> sleepAndLog(Duration.ofSeconds(1)), taskExecutor);
+            CompletableFuture.allOf(task1, task2, task3).join();
         }
     }
 
