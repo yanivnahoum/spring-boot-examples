5c66bbf1c3a0b670cb35f0c3d60017eb3dc723e1
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-07-25 11:12:53 +0300

    Added Service Bus health indicator

---
+++ azure-service-bus/src/main/java/com/att/training/springboot/examples/ServiceBusHealthIndicator.java
@@ -0,0 +1,33 @@
+package com.att.training.springboot.examples;
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClient;
+import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClientBuilder;
+import com.azure.spring.cloud.autoconfigure.implementation.servicebus.properties.AzureServiceBusProperties;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceBusHealthIndicator extends AbstractHealthIndicator {
+    private final ServiceBusAdministrationClient serviceBusAdministrationClient;
+
+    public ServiceBusHealthIndicator(AzureServiceBusProperties azureServiceBusProperties) {
+        super("Service bus health check failed");
+        var tokenCredential = new DefaultAzureCredentialBuilder().build();
+        serviceBusAdministrationClient = new ServiceBusAdministrationClientBuilder()
+                .credential(azureServiceBusProperties.getFullyQualifiedNamespace(), tokenCredential)
+//                .connectionString(azureServiceBusProperties.getConnectionString())
+                .buildClient();
+    }
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) {
+        var namespaceProperties = serviceBusAdministrationClient.getNamespaceProperties();
+        if (namespaceProperties != null) {
+            builder.up().withDetail("namespace", namespaceProperties.getName());
+        } else {
+            builder.down().withDetail("error", "Unable to retrieve namespace properties");
+        }
+    }
+}
