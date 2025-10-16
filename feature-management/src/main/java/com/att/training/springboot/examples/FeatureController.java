bc8e510bf3964c1f18ee25e0c4a86cfb6509a1c6
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-10-16 19:10:53 +0300

    Refactor FeatureController to use ObjectProvider for AppConfigurationRefresh

--- feature-management/src/main/java/com/att/training/springboot/examples/FeatureController.java
+++ feature-management/src/main/java/com/att/training/springboot/examples/FeatureController.java
@@ -4,6 +4,7 @@ import com.azure.spring.cloud.appconfiguration.config.AppConfigurationRefresh;
 import com.azure.spring.cloud.feature.management.FeatureManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class FeatureController {
     private final FeatureManager featureManager;
-    private final AppConfigurationRefresh appConfigurationRefresh;
+    private final ObjectProvider<AppConfigurationRefresh> appConfigurationRefreshProvider;
 
     @GetMapping("check/{feature}")
     Mono<Boolean> isFeatureEnabled(@PathVariable String feature) {
@@ -29,7 +30,8 @@ public class FeatureController {
 
     @GetMapping
     Mono<Set<String>> fetchAll() {
-        return Mono.defer(appConfigurationRefresh::refreshConfigurations)
+        return Mono.justOrEmpty(appConfigurationRefreshProvider.getIfAvailable())
+                .flatMap(AppConfigurationRefresh::refreshConfigurations)
                 .subscribeOn(Schedulers.boundedElastic())
                 .doOnSuccess(refreshed -> log.info("Refreshed all configurations: {}", refreshed))
                 .map(ignored -> featureManager.getAllFeatureNames());
