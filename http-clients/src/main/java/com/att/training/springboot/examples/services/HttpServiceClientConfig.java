8341c438e70c263c2cef7cec0beb0df6bc7103fc
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2026-02-21 20:34:30 +0200

    Add API versioning and authentication token to HttpServiceClientConfig

--- http-clients/src/main/java/com/att/training/springboot/examples/services/HttpServiceClientConfig.java
+++ http-clients/src/main/java/com/att/training/springboot/examples/services/HttpServiceClientConfig.java
@@ -7,6 +7,7 @@ import com.att.training.springboot.examples.services.movie.MovieService;
 import com.att.training.springboot.examples.services.review.RatingService;
 import com.att.training.springboot.examples.services.review.ReviewService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.ApiVersionInserter;
 import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.registry.ImportHttpServices;
 
@@ -19,14 +20,19 @@ public class HttpServiceClientConfig {
 //    @Bean
     RestClientHttpServiceGroupConfigurer httpServiceGroupConfigurer() {
         return groups -> {
-//            groups.filterByName("actor").forEachClient((_, restClientBuilder) ->
-//                    restClientBuilder
-//                            .apiVersionInserter(ApiVersionInserter.useHeader("X-Version"))
-//                            .defaultApiVersion("1.0")
-//            );
+            groups.filterByName("actor").forEachClient((_, restClientBuilder) ->
+                    restClientBuilder
+                            .apiVersionInserter(ApiVersionInserter.useHeader("X-Version"))
+                            .defaultApiVersion("1.0")
+            );
 
             groups.forEachClient((group, restClientBuilder) ->
                     restClientBuilder.defaultHeader("service-group", group.name()));
+
+            var token = "ABCDE-12345-FFFFF-67890";
+            groups.filterByName("movie")
+                    .forEachClient((_, restClientBuilder) ->
+                            restClientBuilder.defaultHeaders(headers -> headers.setBearerAuth(token)));
         };
     }
 }
